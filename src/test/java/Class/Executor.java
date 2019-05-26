f5282c12cdd60907ef07d02b1829badf974c1cbc
Willian Stech <willianstech@gmail.com>
2019-05-26 12:49:38 -0300

    login submarino 1st commit

---
+++ src/test/java/Class/Executor.java
@@ -0,0 +1,20 @@
+package Class;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		plugin = "pretty",
+		monochrome = true,
+		snippets = SnippetType.CAMELCASE,
+		dryRun = false,
+		strict = false,
+		glue = "stepDefinition",
+		features = "features")
+public class Executor {
+
+}
