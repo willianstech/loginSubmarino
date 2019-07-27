dd2ea8fa7676eeb76356d2516c5ea86e1d13793f
Willian <willianstech@gmail.com>
2019-07-27 17:14:13 -0300

    Second Commit

--- src/test/java/Funcoes/Inicial.java
+++ src/test/java/Funcoes/Inicial.java
@@ -7,5 +7,8 @@ public class Inicial extends TelaInicial {
 	public void acessarPaginaInicial() throws Throwable {
 		que_eu_estou_na_pagina_da_casas_bahia();
 		acessar_a_pagina_de_cadastro();
+		
+		
+		
 	}
 }
